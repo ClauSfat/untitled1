9107640615ac5422dea1e55f2d38bd2a2638f432
ClaSfat <claudiasfatuta@gmail.com>
2023-01-24 16:17:26 +0200

    Am adaugat javdoc

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
 
-    public static void main(String[] /* acesta este un parametru  */args) {
-        /* aceasta este in mesaj
+    /**
+     *
+     * @param args Acesta este un parametru
+     * @author Claudia
+     *Putem sa transmitem argumente aditionale <b>args</b>
+     */
+    public static void main(String[] args) {
 
-      Textul face parte din comentariu multiline
-     Aici este un alt exemplu de comentariu multilinie
-         */
+
                 System.out.println("Invat java si masina virtuala java!");
     }
 }
